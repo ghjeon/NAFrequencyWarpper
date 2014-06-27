643a0c443cb4cb93db31e35f61e539786e3b7a7d
wingless <wingless@trinity.so>
2014-06-27 09:09:27 +0900

    Initial Commit

---
+++ src/edu/skku/monet/NAFrequencyWarpper/Sender/FreqencyConstatns.java
@@ -0,0 +1,39 @@
+package edu.skku.monet.NAFrequencyWarpper.Sender;
+
+/**
+ * Project IntelliJ IDEA
+ * Module edu.skku.monet.NAFrequencyWarpper.Sender
+ * User: Gyuhyeon
+ * Date: 2014. 6. 27.
+ * Time: 오전 1:20
+ */
+public class FreqencyConstatns {
+
+    public static final int[] SendAA = {18000, 18000, 18000, 18000};
+    public static final int[] SendAB = {18000, 18000, 18100, 18100};
+    public static final int[] SendAC = {18000, 18000, 18200, 18200};
+    public static final int[] SendAD = {18000, 18000, 18300, 18300};
+    public static final int[] SendAE = {18000, 18000, 18400, 18400};
+    public static final int[] SendAF = {18000, 18000, 18500, 18500};
+    public static final int[] SendAG = {18000, 18000, 18600, 18600};
+    public static final int[] SendAH = {18000, 18000, 18700, 18700};
+    public static final int[] SendAI = {18000, 18000, 18800, 18800};
+    public static final int[] SendAJ = {18000, 18000, 18900, 18900};
+    public static final int[] SendAK = {18000, 18000, 19000, 19000};
+    public static final int[] SendAL = {18000, 18000, 19100, 19100};
+    public static final int[] SendAM = {18000, 18000, 19200, 19200};
+    public static final int[] SendAN = {18000, 18000, 19300, 19300};
+    public static final int[] SendAO = {18000, 18000, 19400, 19400};
+    public static final int[] SendAP = {18000, 18000, 19500, 19500};
+    public static final int[] SendAQ = {18000, 18000, 19600, 19600};
+    public static final int[] SendAR = {18000, 18000, 19700, 19700};
+    public static final int[] SendAS = {18000, 18000, 19800, 19800};
+    public static final int[] SendAT = {18000, 18000, 19900, 19900};
+    public static final int[] SendBC = {18000, 18000, 18200, 18200};
+    public static final int[] SendBD = {18000, 18000, 18300, 18300};
+    public static final int[] SendBE = {18000, 18000, 18400, 18400};
+    public static final int[] SendBF = {18000, 18000, 18500, 18500};
+    public static final int[] SendBG = {18000, 18000, 18600, 18600};
+    public static final int[] SendBH = {18000, 18000, 18700, 18700};
+
+}
