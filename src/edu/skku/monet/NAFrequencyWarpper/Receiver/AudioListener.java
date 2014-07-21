2886c78d30271bf5c9e4dd6dd33cfc980c9e6a78
wingless <wingless@trinity.so>
2014-07-22 01:41:58 +0900

    Implement Receiver

--- src/edu/skku/monet/NAFrequencyWarpper/Receiver/AudioListener.java
+++ src/edu/skku/monet/NAFrequencyWarpper/Receiver/AudioListener.java
@@ -1,7 +1,10 @@
 package edu.skku.monet.NAFrequencyWarpper.Receiver;
 
+import com.badlogic.gdx.audio.analysis.KissFFT;
 import edu.skku.monet.NAFrequencyWarpper.MyActivity;
 import edu.skku.monet.NAFrequencyWarpper.Receiver.*;
+import com.badlogic.gdx.utils.GdxNativesLoader;
+import com.badlogic.gdx.audio.analysis.FFT;
 import android.os.*;
 import android.media.*;
 import android.util.Log;
@@ -14,7 +17,7 @@ import android.util.Log;
  * Date: 2014. 7. 17.
  * Time: 오후 11:49
  */
-public class AudioListener extends AsyncTask<Void, double[], Void>{
+public class AudioListener extends AsyncTask<Void, Void, Void>{
 
     public static final int kFFTSIZE = 32768;
     public static final int kBUFFERSIZE = 32768;
@@ -23,10 +26,16 @@ public class AudioListener extends AsyncTask<Void, double[], Void>{
 
     Object[] d = new Object[2];
 
+    private float[] freq_f;
     private double[] freq_db;
     private double[] freq_db_mix;
     private double[] freq_db_harmonic;
+    float[] fft_cpx, tmpi, tmpr;
+    double[] real = new double[kFFTSIZE / 2];
+    double[] imag= new double[kFFTSIZE / 2];
     double[] cToTransform = new double[kFFTSIZE];
+
+    FFT fft = new FFT(kFFTSIZE, kSAMPLERATE);
 
     @Override
     protected Void doInBackground(Void... params) {
@@ -34,17 +43,15 @@ public class AudioListener extends AsyncTask<Void, double[], Void>{
         try
         {
             // AudioRecord를 설정하고 사용한다.
+            freq_f = new float[kFFTSIZE];
             freq_db = new double[kFFTSIZE/2];
             freq_db_mix = new double[kFFTSIZE/2];
             freq_db_harmonic = new double[kFFTSIZE/2];
-            int bufferSize = AudioRecord.getMinBufferSize(l.frequency, l.channelConfiguration, l.audioEncoding);
+            int bufferSize = AudioRecord.getMinBufferSize(kSAMPLERATE, l.channelConfiguration, l.audioEncoding);
 
             AudioRecord audioRecord = new AudioRecord(
-            MediaRecorder.AudioSource.MIC, l.frequency, l.channelConfiguration, l.audioEncoding, bufferSize);
+            MediaRecorder.AudioSource.MIC, kSAMPLERATE, l.channelConfiguration, l.audioEncoding, bufferSize);
 
-
-                // short로 이뤄진 배열인 buffer는 원시 PCM 샘플을 AudioRecord 객체에서 받는다.
-                // double로 이뤄진 배열인 toTransform은 같은 데이터를 담지만 double 타입인데, FFT 클래스에서는 double타입이 필요해서이다.
             short[] buffer = new short[l.blockSize];
             double[] toTransform = new double[l.blockSize];
 
@@ -53,30 +60,15 @@ public class AudioListener extends AsyncTask<Void, double[], Void>{
             while(MyActivity.started){
                 int bufferReadResult = audioRecord.read(buffer, 0, l.blockSize);
 
-                    // AudioRecord 객체에서 데이터를 읽은 다음에는 short 타입의 변수들을 double 타입으로 바꾸는 루프를 처리한다.
-                    // 직접 타입 변환(casting)으로 이 작업을 처리할 수 없다. 값들이 전체 범위가 아니라 -1.0에서 1.0 사이라서 그렇다
-                    // short를 32,768.0(Short.MAX_VALUE) 으로 나누면 double로 타입이 바뀌는데, 이 값이 short의 최대값이기 때문이다.
                 for(int i = 0; i < l.blockSize && i < bufferReadResult; i++){
-                    toTransform[i] = (double)buffer[i] / Short.MAX_VALUE; // 부호 있는 16비트
+                    freq_f[i] = buffer[i];
                 }
-                cToTransform = toTransform;
-                    // 이제 double값들의 배열을 FFT 객체로 넘겨준다. FFT 객체는 이 배열을 재사용하여 출력 값을 담는다. 포함된 데이터는 시간 도메인이 아니라
-                    // 주파수 도메인에 존재한다. 이 말은 배열의 첫 번째 요소가 시간상으로 첫 번째 샘플이 아니라는 얘기다. 배열의 첫 번째 요소는 첫 번째 주파수 집합의 레벨을 나타낸다.
 
-                    // 256가지 값(범위)을 사용하고 있고 샘플 비율이 8,000 이므로 배열의 각 요소가 대략 15.625Hz를 담당하게 된다. 15.625라는 숫자는 샘플 비율을 반으로 나누고(캡쳐할 수 있는
-                    // 최대 주파수는 샘플 비율의 반이다. <- 누가 그랬는데...), 다시 256으로 나누어 나온 것이다. 따라서 배열의 첫 번째 요소로 나타난 데이터는 영(0)과 15.625Hz 사이에
-                    // 해당하는 오디오 레벨을 의미한다.
-                l.transformer.ft(toTransform);
-
-                    // publishProgress를 호출하면 onProgressUpdate가 호출된다.
-                publishProgress(toTransform);
-                try
-                {
-                    Thread.sleep(100);
-                } catch (Exception e)
-                {
-
-                }
+                fft.forward(freq_f);
+                fft_cpx = fft.getSpectrum();
+                tmpi = fft.getImaginaryPart();
+                tmpr = fft.getRealPart();
+                publishProgress();
             }
 
             audioRecord.stop();
@@ -89,44 +81,27 @@ public class AudioListener extends AsyncTask<Void, double[], Void>{
 
 
     @Override
-    protected void onProgressUpdate(double[]... toTransform) {
+    protected void onProgressUpdate(Void... params) {
         for(int i = 0; i < kFFTSIZE/2; i++)
         {
-            freq_db[i] = Math.sqrt((double)toTransform[0][i*2]*10000 * (double)toTransform[0][i*2]*10000 +
-                    (double)toTransform[0][i*2+1]*10000 * (double)toTransform[0][i*2+1]*10000) / (kFFTSIZE/2);
+            real[i] = (double)tmpr[i];
+            imag[i] = (double)tmpi[i];
+            freq_db[i] = Math.sqrt((real[i]*real[i]) + (imag[i]*imag[i]));
         }
         addHarmonics();
 
-        /*
-        for(int i = 0; i < toTransform[0].length; i++){
-            int x = i;
-            int downy = (int) (100 - (toTransform[0][i] * 10));
-            int upy = 100;
-            int hz = (int)(toTransform[0][i]*100);
-            if(hz > 1000)
-                Log.e("[aListener]", hz + "Hz");
-        }
-        */
+        int max = frequencyIndex();
+        boolean checka = checkFreq(25, max);
+        boolean checkb = checkFreq(30, max);
+        Log.e("[checkA]", "RESULT :: " + checka);
+        Log.e("[checkB]", "RESULT :: " + checkb);
         Log.e("[aListener]", "MAX:: " + frequencyIndex() + "Hz");
-    }
-
-    protected double hammingWindow(int totalSamples, int input) {
-        double a = 2.0 * 3.141592654 / ( totalSamples - 1 );
-        double w;
-
-        w = 0.54 - 0.46 * Math.cos( a * input );
-        return w;
-    }
-
-    protected void performWindow(short[] buffer, int totalSamples) {
-        for (int i = 0; i < totalSamples; i++){
-            buffer[i] *= hammingWindow(totalSamples, i);
-        }
     }
 
     protected void addHarmonics() {
         int max_harmonics = 3;
         int fftrange = kFFTSIZE/2;
+        freq_db_harmonic = freq_db.clone();
         for(int j = 2; j <= max_harmonics; j++){
             int low_bin = 0;
             double new_value = 0;
@@ -156,7 +131,6 @@ public class AudioListener extends AsyncTask<Void, double[], Void>{
         double max = 0;
         int max_index = 0;
         for(int i = 0; i < kHighSIZE; i++){
-//		double db = freq_db_harmonic[i];
             double db = freq_db_mix[i];
             if(db > max){
                 max = db;
@@ -176,8 +150,6 @@ public class AudioListener extends AsyncTask<Void, double[], Void>{
 
     protected int frequencyIndex() {
         int freq_index = findTopSpike();
-
-        // 인덱스를 더 잘 찾기 위해 앞에꺼와 뒤에꺼를 더해서
 
         if (freq_db_mix[freq_index] > 10) {
             return freq_index;
